c9f255ba22625644c5d3dc1780afb4ad9f88c66e
Jordan Papaleo <jordan.papaleo@clearcapital.com>
2018-05-18 21:17:34 -0700

    package updates

--- server/services/api.js
+++ server/services/api.js
@@ -1,45 +1,74 @@
-import axios from 'axios'
+export default class FetchApi {
+  static timeout = 6000
 
-export default class Api {
   static headers = {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json;charser=utf-8'
+    'Accept': 'application/json, text/javascript; q=0.9, */*; q=0.6',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Content-Type': 'application/json'
   }
 
-  static timeout = 120000
-
-  constructor (baseUrl) {
+  constructor (props) {
+    const {baseUrl, headers, errorHandler} = props
+    this.errorHandler = errorHandler
     this.baseUrl = baseUrl
-  }
+    this.headers = {
+      ...FetchApi.headers
+    }
 
-  request (url, options, passThrough) {
-    return fetch(url, options)
-      .then((response) => {
-        console.log('response', response)
-
-        if (response.statusText === 'OK') {
-          let data = response.data
-
-          if (passThrough) {
-            data = {
-              ...passThrough,
-              ...response.data
-            }
-          }
-
-          return data
+    if (headers) {
+      Object.keys(headers).forEach((key) => {
+        if (headers[key] === null) {
+          delete this.headers[key]
         } else {
-          throw new Error(`${response.status} ${response.statusText}`)
+          this.headers[key] = headers[key]
         }
       })
+    }
   }
 
-  get (url, passThrough, timeout = Api.timeout) {
-    const {headers} = Api
-    const {request, baseUrl} = this
+  request = async (url, options, passThrough = {}) => {
+    const {errorHandler} = this
+
+    const response = await fetch(`${url}`, options).catch((err) => {
+      if (errorHandler) {
+        errorHandler(err)
+      } else {
+        throw err
+      }
+    })
+
+    if (response.ok) {
+      if (response.status !== 204) {
+        const promise = (response.headers.get('content-type') === 'image/jpeg')
+          ? response.text()
+          : response.json()
+
+        const data = await promise().catch((err) => {
+          console.log('data process err', err)
+        })
+
+        return (passThrough)
+          ? {...passThrough, ...data}
+          : data
+      }
+    } else {
+      const apiError = new Error('Response not ok')
+      apiError.response = response
+
+      if (errorHandler) {
+        errorHandler(apiError)
+      } else {
+        throw apiError
+      }
+    }
+  }
+
+  get = (url, passThrough, timeout = FetchApi.timeout) => {
+    const {headers, request, baseUrl} = this
     const options = {
       timeout,
-      headers: headers,
+      headers,
+      credentials: 'include',
       method: 'GET',
       mode: 'cors'
     }
@@ -47,13 +76,13 @@ export default class Api {
     return request(`${baseUrl}/${url}`, options, passThrough)
   }
 
-  post (url, data, passThrough, timeout = Api.timeout) {
-    const {headers} = Api
-    const {request, baseUrl} = this
+  post = (url, data, passThrough, timeout = FetchApi.timeout) => {
+    const {headers, request, baseUrl} = this
     const options = {
       timeout,
       headers,
-      body: JSON.stringify(data),
+      credentials: 'include',
+      body: this.headers['Content-Type'] === 'application/json' ? JSON.stringify(data) : data,
       method: 'POST',
       mode: 'cors'
     }
@@ -61,13 +90,13 @@ export default class Api {
     return request(`${baseUrl}/${url}`, options, passThrough)
   }
 
-  put (url, data, passThrough, timeout = Api.timeout) {
-    const {headers} = Api
-    const {request, baseUrl} = this
+  put = (url, data, passThrough, timeout = FetchApi.timeout) => {
+    const {headers, request, baseUrl} = this
     const options = {
       timeout,
       headers,
-      body: JSON.stringify(data),
+      credentials: 'include',
+      body: this.headers['Content-Type'] === 'application/json' ? JSON.stringify(data) : data,
       method: 'PUT',
       mode: 'cors'
     }
@@ -75,12 +104,12 @@ export default class Api {
     return request(`${baseUrl}/${url}`, options, passThrough)
   }
 
-  delete (url, passThrough, timeout = Api.timeout) {
-    const {headers} = Api
-    const {request, baseUrl} = this
+  delete = (url, passThrough, timeout = FetchApi.timeout) => {
+    const {headers, request, baseUrl} = this
     const options = {
       timeout,
       headers,
+      credentials: 'include',
       method: 'DELETE',
       mode: 'cors'
     }
